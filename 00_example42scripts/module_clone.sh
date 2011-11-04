58b1fc701585bee0a3e0637ca32d7b9bce1f886e
Alessandro Franceschi <al@lab42.it>
2011-11-05 00:14:37 +0100

    Modified module_clone.sh script to manage different source modules templates

--- 00_example42scripts/module_clone.sh
+++ 00_example42scripts/module_clone.sh
@@ -1,18 +1,42 @@
 #!/bin/bash
 
-echo "This script creates a skeleton for a new module based on the"
-echo "Example42 foo module template"
-echo "Run it from the directory that contains the foo module (moduledir)"
-echo
+showhelp () {
+cat << EOF
 
-if [ ! -f foo/manifests/init.pp ] ; then
-    echo "I don't find "foo/manifests/init.pp" , run this script from the base modules directory"
+This script creates a skeleton for a new module based on different Example42 foo module templates.
+Run it from the directory that contains the foo module (moduledir).
+
+By default it uses the "foo" module as template. Specify -t <source_module> to use a different template.
+
+Example:
+$0 -t foo-app
+
+EOF
+}
+
+template="foo"
+
+while [ $# -gt 0 ]; do
+  case "$1" in
+    -t)
+      template=$2
+      shift 2 ;;
+  esac
+done
+
+showhelp
+
+if [ ! -f $template/manifests/init.pp ] ; then
+    echo "I don't find $template/manifests/init.pp "
+    echo "Run this script from the base modules directory and specify a valid source module template"
     exit 1
 fi
 
-OLDMODULE="foo"
+OLDMODULE="$template"
+OLDMODULESTRING="$template"
 echo
-echo -n "Enter the name of the new module: "
+echo "Source module template is $template "
+echo -n "Enter the name of the new module based on $template: "
 read NEWMODULE
 
 echo "COPYING MODULE"
@@ -21,16 +45,16 @@ rsync -av --exclude=".git" $OLDMODULE/* $NEWMODULE/
 
 
 echo "RENAMING FILES"
-for file in $( find $NEWMODULE | grep $OLDMODULE ) ; do 
-	newfile=`echo $file | sed -e "s/$OLDMODULE/$NEWMODULE/"`
+for file in $( find $NEWMODULE | grep $OLDMODULESTRING ) ; do 
+	newfile=`echo $file | sed -e "s/$OLDMODULESTRING/$NEWMODULE/"`
 	mv "$file" "$newfile" && echo "Renamed $file to $newfile"
 done
 
 echo "---------------------------------------------------"
 echo "CHANGING FILE CONTENTS"
-for file in $( grep -R $OLDMODULE $NEWMODULE | cut -d ":" -f 1 | uniq ) ; do 
-	sed -i "s/$OLDMODULE/$NEWMODULE/g" $file && echo "Changed $file" # Use under Linux
-	# sed -i "" -e "s/$OLDMODULE/$NEWMODULE/g" $file && echo "Changed $file" # Use under MacOS
+for file in $( grep -R $OLDMODULESTRING $NEWMODULE | cut -d ":" -f 1 | uniq ) ; do 
+	sed -i "s/$OLDMODULESTRING/$NEWMODULE/g" $file && echo "Changed $file" # Use under Linux
+	# sed -i "" -e "s/$OLDMODULESTRING/$NEWMODULE/g" $file && echo "Changed $file" # Use under MacOS
 done
 
 echo "Module $NEWMODULE created"
