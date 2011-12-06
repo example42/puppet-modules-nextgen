cc871887bddc344720d7fee87c38b801b9d58bf9
Alessandro Franceschi <al@lab42.it>
2011-12-06 01:25:26 +0100

    Updated modules. Still testing base layouts.

--- role/manifests/test.pp
+++ role/manifests/test.pp
@@ -5,7 +5,8 @@ class role::test {
   class { "solr" :
 #    install    => "package",
 #    install_source => "http://www.apache.org/dist/lucene/solr/3.3.0/apache-solr-3.3.0.tgz",
-#    install => "source",
+    install_source => "http://10.42.42.10/apache-solr-3.4.0.tar.gz",
+    install => "puppi",
     source_dir => "puppet:///modules/example42/solr/",
     puppi      => true,
   }
